db8f3590513b6f3d3e12d836830a289a59b096f2
Memiru <48900359+Memiru@users.noreply.github.com>
2022-12-21 18:27:15 +0900

    20221221 web init
    Web Init

---
+++ precache-manifest.c64551c6fb5286b3a1491873d10c65f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "be30590fd0b14fd7be3c",
+    "url": "/MushroomWeb/static/css/main.cacde6ce.chunk.css"
+  },
+  {
+    "revision": "be30590fd0b14fd7be3c",
+    "url": "/MushroomWeb/static/js/main.be30590f.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/MushroomWeb/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "c2d6b731324c1ef7ad88",
+    "url": "/MushroomWeb/static/js/2.c2d6b731.chunk.js"
+  },
+  {
+    "revision": "b37b3c25c32c9f2ce324",
+    "url": "/MushroomWeb/static/js/runtime~main.b37b3c25.js"
+  },
+  {
+    "revision": "fb932d042f385927ba59e9a690538709",
+    "url": "/MushroomWeb/static/media/MaplestoryLight.fb932d04.ttf"
+  },
+  {
+    "revision": "b0a6a2693556296fa674f5e4de61810b",
+    "url": "/MushroomWeb/static/media/MaplestoryBold.b0a6a269.ttf"
+  },
+  {
+    "revision": "b7832bbdef79a499635f90d3b365cc4c",
+    "url": "/MushroomWeb/index.html"
+  }
+];
